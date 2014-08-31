288f2957d8f9e2a224bd7c325808325e91ba2cb6
neil butterworth <neilb@cordelia>
2014-08-31 14:03:33 +0100

    squash code compiling (untested)

--- csvfix/inc/csved_squash.h
+++ csvfix/inc/csved_squash.h
@@ -13,8 +13,21 @@
 #include "csved_command.h"
 #include <map>
 #include <vector>
+#include <string>
+#include <memory>
 
 namespace CSVED {
+
+
+class SquashValues {
+
+    public:
+
+        SquashValues() {}
+        virtual ~SquashValues() {}
+        virtual void Add( unsigned int idx, const std::string & val ) = 0;
+        virtual void AppendTo( CSVRow & row ) = 0;
+};
 
 //---------------------------------------------------------------------------
 
@@ -30,6 +43,15 @@ class SquashCommand : public Command {
 
 	private:
 
+        void ProcessFlags( const ALib::CommandLine & cmd );
+        std::string MakeKey( const CSVRow & row ) const;
+        void Accumulate( std::shared_ptr<SquashValues>& vp, const CSVRow & row  );
+        void Key2CSV( const std::string & key, CSVRow & row);
+
+        FieldList mKeyFields, mValFields;
+        std::map <std::string, std::shared_ptr<SquashValues>> mKeyValues;
+        std::string mNotNumVal;
+        bool mRealNums;
 
 };
 
